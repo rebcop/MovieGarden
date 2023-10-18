a2101052ae4cf17c939db19edfa29ec444cbf00d
rebcop <76892971+rebcop@users.noreply.github.com>
2023-10-18 14:50:47 -0400

    Created modal which brings in info

--- js/site.js
+++ js/site.js
@@ -24,6 +24,30 @@ async function getMovies() {
     }
 }
 
+async function getMovie(movieId) {
+
+        // attempt to run this code, used when incorporating information from other people so it doesn't break our code, not for your own code that you can control
+    try {
+
+                        // waiting for asyncronous operation to go to a url and comes back with a response
+        let response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}`, {
+            headers: {
+                'Authorization': `Bearer ${API_KEY}`
+            }
+        });
+
+        // changes the string recieved back and parses it into JSON object which is a javascript object in javascript
+        let data = await response.json();
+
+        return data;
+
+        // if something goes wrong, run this backup code
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
 // Needs to wait for getMovies so have to use async in front of function
 async function displayMovies() {
 
@@ -39,21 +63,58 @@ async function displayMovies() {
 
             let movie = movies[i];
 
-            // Get all the child elements to copy over as well when cloneNode(true) if false, then no child elements
+            // Get all the child elements to copy over as well when cloneNode(true), if false instead of true is used, then no child elements
             let movieCard = moviePosterTemplate.content.cloneNode(true);
 
-            // Uses a CSS selector to get the element
+            // Uses a CSS selector to get the element and reassign the value of the img src attribute
             let movieImgElement = movieCard.querySelector('.card-img-top');
             movieImgElement.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
 
-            // assigns img src attribute
+            // Get the element holding the movie title and replace the text with the title of the movie
             let movieTitleElement = movieCard.querySelector('.card-body > h5');
             movieTitleElement.textContent = movie.title;
 
-            // replaces paragraph with summary of
+            // Get the element holding the paragraph information and replace the text with the summary of movie
             let movieParagraphElement = movieCard.querySelector('.card-text');
             movieParagraphElement.textContent = movie.overview;
 
+            // Make up own attribute to assign movie id to access this information later
+            let movieButton = movieCard.querySelector('.btn-primary');
+            movieButton.setAttribute('data-movieId', movie.id);
+
+            // Add the revised card to the page
             movieListDiv.appendChild(movieCard);
         } 
+}
+
+
+async function showMovieDetails(clickedBtn) {
+
+    // get the ID of the movie that was clicked
+    let movieId = clickedBtn.getAttribute('data-movieId');
+
+    // TODO:
+    // get the details of the movie with that ID from TMBD API
+    let movieData = await getMovie(movieId);
+
+    // Modify Content in Headaer of Modal
+    let modalTitle = document.querySelector('#movieModal .modal-title');
+    modalTitle.textContent = movieData.title;
+
+    let modalTagline = document.querySelector('#movieModal .tagline');
+    modalTagline.textContent = `TAGLINE: ${movieData.tagline}`;
+
+    // Modify Content in Body of Modal
+    let modalBody = document.querySelector('#movieModal .modal-body');
+    modalBody.textContent = movieData.overview;
+        
+    // Modify Content in Footer of Modal
+    let modalFooter = document.querySelector('#movieModal .modal-footer');
+
+    let moviePageBtn = document.querySelector('#movieModal .btn-primary');
+    moviePageBtn.href = movieData.homepage;
+
+    // WANT: collection(maybe after), genres[], original_language, popularity, poster_path
+    // release date, runtime, vote avg, vote count
+
 }
